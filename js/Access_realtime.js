7b5e1132250282bef4add5c543d66b125ea26f83
amhadid <134684276+amhadid@users.noreply.github.com>
2024-04-26 01:56:38 +0700

    Add files via upload

---
+++ js/Access_realtime.js
@@ -0,0 +1,57 @@
+//Access Real-time Position
+if(!navigator.geolocation) {
+    console.log("Your browser doesn't support geolocation feature!")
+} else {
+    setInterval(() => {
+        navigator.geolocation.getCurrentPosition(getPosition)
+    }, 10000);
+}
+
+var marker, circle;
+
+function getPosition(position){
+    // console.log(position)
+    var lat = position.coords.latitude
+    var long = position.coords.longitude
+    var accuracy = position.coords.accuracy
+
+    if(marker) {
+        map.removeLayer(marker)
+    }
+
+    if(circle) {
+        map.removeLayer(circle)
+    }
+
+    // Define icon options
+    var iconOptions = {
+        iconUrl: './Assets/img/user-icon.png',
+        iconSize: [28, 32], // Sesuaikan ukuran ikon
+        iconAnchor: [16, 32], // Sesuaikan anchor ikon
+    };
+
+    // Create custom icon
+    var customIcon = L.icon(iconOptions);
+
+    // Create marker with custom icon
+    marker = L.marker([lat, long], { icon: customIcon });
+
+    // Create circle
+    circle = L.circle([lat, long], { radius: 10 });
+
+    // Add marker and circle to a feature group
+    var featureGroup = L.featureGroup([marker, circle]).addTo(map);
+
+    console.log("Your Location is: Lat: "+ lat +" Long: "+ long+ " Accuracy: "+ accuracy)
+}
+
+// If Cannot Access Geolocation
+function error(err) {
+    if (err.code === 1) {
+        alert("Please allow geolocation access");
+        // Runs if user refuses access
+    } else {
+        alert("Cannot get current location");
+        // Runs if there was a technical problem.
+    }
+}
