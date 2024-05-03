ba217efcf80c375261a23ab8c53e60a69dae7e88
amhadid <134684276+amhadid@users.noreply.github.com>
2024-05-03 21:53:36 +0700

    Add files via upload

---
+++ js/api.js
@@ -0,0 +1,21 @@
+// Connect Server
+fetch('http://localhost:4000/api/user')
+    .then(response => response.json())
+    .then(({ areaSemarang, areaRawan, kecelakaan2021, kecelakaan2022, kecelakaan2023 }) => {
+        console.log("Area Kota Semarang:");
+        console.log(areaSemarang);
+
+        console.log("Area Rawan:");
+        console.log(areaRawan);
+
+        console.log("Kecelakaan 2021:");
+        console.log(kecelakaan2021);
+
+        console.log("Kecelakaan 2022:");
+        console.log(kecelakaan2022);
+
+        console.log("Kecelakaan 2023:");
+        console.log(kecelakaan2023);
+    })
+    .catch(error => console.error('Error:', error));
+
